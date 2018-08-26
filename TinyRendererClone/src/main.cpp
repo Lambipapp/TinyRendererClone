1e5d540ce1ba4e39734b06adbecd8ed6fa145f62
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-08-26 02:36:14 +0200

    head rendered in wireframe

--- TinyRendererClone/src/main.cpp
+++ TinyRendererClone/src/main.cpp
@@ -1,6 +1,8 @@
 #include <provided/tgaimage.h>
 #include <provided/model.h>
 #include <provided/geometry.h>
+#include <vector>
+#include <cmath>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -53,26 +55,21 @@ int main(int argc, char** argv) {
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
-	//image.set(52, 41, red);
-	//line(20, 20, 50, 50, image, white);
-	//line(13, 20, 80, 40, image, white);
-	//line(20, 13, 40, 80, image, red);
-	//line(80, 40, 13, 20, image, red);
-	//for (int i = 0; i < model->nfaces(); i++) {
-	//	std::vector<int> face = model->face(i);
-	//	for (int j = 0; j < 3; j++) {
-	//		Vec3f v0 = model->vert(face[j]);
-	//		Vec3f v1 = model->vert(face[(j + 1) % 3]);
-	//		int x0 = (v0.x + 1.)*width / 2.;
-	//		int y0 = (v0.y + 1.)*height / 2.;
-	//		int x1 = (v1.x + 1.)*width / 2.;
-	//		int y1 = (v1.y + 1.)*height / 2.;
-	//		line(x0, y0, x1, y1, image, white);
-	//	}
-	//}
 
+	for (int i = 0; i < model->nfaces(); i++) {
+		vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0, x1, y0, y1;
+			x0 = (v0.x + 1.0f) * width / 2.0f;
+			y0 = (v0.y + 1.0f) * height / 2.0f;
+			x1 = (v1.x + 1.0f) * width / 2.0f;
+			y1 = (v1.y + 1.0f) * height / 2.0f;
+			line(x0, y0, x1, y1, image, red);
+		}
+	}
 	
-
 
 	image.flip_vertically(); // origin placed int the left bottom corner of the image
 	image.write_tga_file("output.tga"); 
